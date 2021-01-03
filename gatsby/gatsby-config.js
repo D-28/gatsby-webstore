2f25aee3340c01f679738249ddb16060faeca7e2
Dan Shkolnik <dshko@c02wx6z4jg5h.cable.rcn.com>
2021-01-03 17:28:19 -0500

    first commit

---
+++ gatsby/gatsby-config.js
@@ -0,0 +1,24 @@
+import dotenv from 'dotenv';
+
+dotenv.config({ path: '.env' });
+
+export default {
+  siteMetadata: {
+    title: 'Slicks Slices',
+    siteUrl: 'https://gatsby.pizza',
+    description: 'The best pizza place in Hamilton!',
+  },
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      // this is the name of the plugin you are adding
+      resolve: 'gatsby-source-sanity',
+      options: {
+        projectId: 'g36fyp7w',
+        dataset: 'production',
+        watchMode: true,
+        token: process.env.SANITY_TOKEN,
+      },
+    },
+  ],
+};
---
+++ gatsby/gatsby-node.js
@@ -0,0 +1,65 @@
+import path from 'path';
+import fetch from 'isomorphic-fetch';
+
+async function turnPizzasIntoPages({ graphql, actions }) {
+  const pizzaTemplate = path.resolve('./src/templates/Pizza.js');
+  const { data } = await graphql(`
+    query {
+      pizzas: allSanityPizza {
+        nodes {
+          name
+          slug {
+            current
+          }
+        }
+      }
+    }
+  `);
+  data.pizzas.nodes.forEach((pizza) => {
+    actions.createPage({
+      path: `pizza/${pizza.slug.current}`,
+      component: pizzaTemplate,
+      context: {
+        slug: pizza.slug.current,
+      },
+    });
+  });
+}
+
+async function turnToppingsIntoPages({ graphql, actions }) {
+  const toppingsTemplate = path.resolve('./src/pages/pizzas.js');
+  const { data } = await graphql(`
+    query {
+      toppings: allSanityTopping {
+        nodes {
+          name
+          id
+        }
+      }
+    }
+  `);
+  data.toppings.nodes.forEach((topping) => {
+    actions.createPage({
+      path: `topping/${topping.name}`,
+      component: toppingsTemplate,
+      context: {
+        topping: topping.name,
+        toppingRegex: `/${topping.name}/i`,
+      },
+    });
+  });
+}
+
+async function fetchBeersAndTurnIntoNodes({ actions, createNodeId, createContentDigest }) {
+  const res = await fetch('https://sampleapis.com/beers/api/ale');
+  const beers = await res.json();
+  console.log('🍺', beers);
+}
+
+export async function sourceNodes(params) {
+  await Promise.all([fetchBeersAndTurnIntoNodes(params)]);
+}
+
+export async function createPages(params) {
+  await Promise.all([turnPizzasIntoPages(params), turnToppingsIntoPages(params)]);
+}
